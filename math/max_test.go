bb6da131ff4554cec718d04a6ff4bd1abe8d94cd
賀川　諒 <ryo.kagawa.217@gmail.com>
2021-03-24 01:28:38 +0900

    bugfix: テストケース修正

--- math/max_test.go
+++ math/max_test.go
@@ -59,73 +59,73 @@ func Test_max_Interface(t *testing.T) {
 }
 
 func Test_max_Float32(t *testing.T) {
-	if reflect.DeepEqual(Max.Float32(1), float32(1)) {
+	if !reflect.DeepEqual(Max.Float32(1), float32(1)) {
 		t.Errorf("max.Float32(1) = float32(1)")
 	}
 }
 
 func Test_max_Float64(t *testing.T) {
-	if reflect.DeepEqual(Max.Float64(1), float64(1)) {
+	if !reflect.DeepEqual(Max.Float64(1), float64(1)) {
 		t.Errorf("max.Float64(1) = float64(1)")
 	}
 }
 
 func Test_max_Int(t *testing.T) {
-	if reflect.DeepEqual(Max.Int(1), int(1)) {
+	if !reflect.DeepEqual(Max.Int(1), int(1)) {
 		t.Errorf("max.Int(1) = int(1)")
 	}
 }
 
 func Test_max_Int8(t *testing.T) {
-	if reflect.DeepEqual(Max.Int8(1), int8(1)) {
+	if !reflect.DeepEqual(Max.Int8(1), int8(1)) {
 		t.Errorf("max.Int8(1) = int8(1)")
 	}
 }
 
 func Test_max_Int16(t *testing.T) {
-	if reflect.DeepEqual(Max.Int16(1), int16(1)) {
+	if !reflect.DeepEqual(Max.Int16(1), int16(1)) {
 		t.Errorf("max.Int16(1) = int16(1)")
 	}
 }
 
 func Test_max_Int32(t *testing.T) {
-	if reflect.DeepEqual(Max.Int32(1), int32(1)) {
+	if !reflect.DeepEqual(Max.Int32(1), int32(1)) {
 		t.Errorf("max.Int32(1) = int32(1)")
 	}
 }
 
 func Test_max_Int64(t *testing.T) {
-	if reflect.DeepEqual(Max.Int64(1), int64(1)) {
+	if !reflect.DeepEqual(Max.Int64(1), int64(1)) {
 		t.Errorf("max.Int64(1) = int64(1)")
 	}
 }
 
 func Test_max_UInt(t *testing.T) {
-	if reflect.DeepEqual(Max.UInt(1), uint(1)) {
+	if !reflect.DeepEqual(Max.UInt(1), uint(1)) {
 		t.Errorf("max.UInt(1) = uint(1)")
 	}
 }
 
 func Test_max_UInt8(t *testing.T) {
-	if reflect.DeepEqual(Max.UInt8(1), uint8(1)) {
+	if !reflect.DeepEqual(Max.UInt8(1), uint8(1)) {
 		t.Errorf("max.UInt8(1) = uint8(1)")
 	}
 }
 
 func Test_max_UInt16(t *testing.T) {
-	if reflect.DeepEqual(Max.UInt16(1), uint16(1)) {
+	if !reflect.DeepEqual(Max.UInt16(1), uint16(1)) {
 		t.Errorf("max.UInt16(1) = uint16(1)")
 	}
 }
 
 func Test_max_UInt32(t *testing.T) {
-	if reflect.DeepEqual(Max.UInt32(1), uint32(1)) {
+	if !reflect.DeepEqual(Max.UInt32(1), uint32(1)) {
 		t.Errorf("max.UInt32(1) = uint32(1)")
 	}
 }
 
 func Test_max_UInt64(t *testing.T) {
-	if reflect.DeepEqual(Max.UInt64(1), uint64(1)) {
+	if !reflect.DeepEqual(Max.UInt64(1), uint64(1)) {
 		t.Errorf("max.UInt64(1) = uint64(1)")
 	}
 }
--- math/min_test.go
+++ math/min_test.go
@@ -30,7 +30,7 @@ func Test_min_Interface(t *testing.T) {
 				value:     int(20),
 				valueList: []int{10},
 			},
-			want: int(20),
+			want: int(10),
 		},
 		{
 			name: "func(int(10), []int{20}) == int(20)",
@@ -38,15 +38,15 @@ func Test_min_Interface(t *testing.T) {
 				value:     int(10),
 				valueList: []int{20},
 			},
-			want: int(20),
+			want: int(10),
 		},
 		{
-			name: "func(int(10), []int{20, 30, 25}) == int(30)",
+			name: "func(int(25), []int{20, 10, 30}) == int(30)",
 			args: args{
-				value:     int(10),
-				valueList: []float64{20, 30, 25},
+				value:     int(25),
+				valueList: []float64{20, 10, 30},
 			},
-			want: int(30),
+			want: int(10),
 		},
 	}
 	for _, tt := range tests {
@@ -59,73 +59,73 @@ func Test_min_Interface(t *testing.T) {
 }
 
 func Test_min_Float32(t *testing.T) {
-	if reflect.DeepEqual(Min.Float32(1), float32(1)) {
+	if !reflect.DeepEqual(Min.Float32(1), float32(1)) {
 		t.Errorf("min.Float32(1) = float32(1)")
 	}
 }
 
 func Test_min_Float64(t *testing.T) {
-	if reflect.DeepEqual(Min.Float64(1), float64(1)) {
+	if !reflect.DeepEqual(Min.Float64(1), float64(1)) {
 		t.Errorf("min.Float64(1) = float64(1)")
 	}
 }
 
 func Test_min_Int(t *testing.T) {
-	if reflect.DeepEqual(Min.Int(1), int(1)) {
+	if !reflect.DeepEqual(Min.Int(1), int(1)) {
 		t.Errorf("min.Int(1) = int(1)")
 	}
 }
 
 func Test_min_Int8(t *testing.T) {
-	if reflect.DeepEqual(Min.Int8(1), int8(1)) {
+	if !reflect.DeepEqual(Min.Int8(1), int8(1)) {
 		t.Errorf("min.Int8(1) = int8(1)")
 	}
 }
 
 func Test_min_Int16(t *testing.T) {
-	if reflect.DeepEqual(Min.Int16(1), int16(1)) {
+	if !reflect.DeepEqual(Min.Int16(1), int16(1)) {
 		t.Errorf("min.Int16(1) = int16(1)")
 	}
 }
 
 func Test_min_Int32(t *testing.T) {
-	if reflect.DeepEqual(Min.Int32(1), int32(1)) {
+	if !reflect.DeepEqual(Min.Int32(1), int32(1)) {
 		t.Errorf("min.Int32(1) = int32(1)")
 	}
 }
 
 func Test_min_Int64(t *testing.T) {
-	if reflect.DeepEqual(Min.Int64(1), int64(1)) {
+	if !reflect.DeepEqual(Min.Int64(1), int64(1)) {
 		t.Errorf("min.Int64(1) = int64(1)")
 	}
 }
 
 func Test_min_UInt(t *testing.T) {
-	if reflect.DeepEqual(Min.UInt(1), uint(1)) {
+	if !reflect.DeepEqual(Min.UInt(1), uint(1)) {
 		t.Errorf("min.UInt(1) = uint(1)")
 	}
 }
 
 func Test_min_UInt8(t *testing.T) {
-	if reflect.DeepEqual(Min.UInt8(1), uint8(1)) {
+	if !reflect.DeepEqual(Min.UInt8(1), uint8(1)) {
 		t.Errorf("min.UInt8(1) = uint8(1)")
 	}
 }
 
 func Test_min_UInt16(t *testing.T) {
-	if reflect.DeepEqual(Min.UInt16(1), uint16(1)) {
+	if !reflect.DeepEqual(Min.UInt16(1), uint16(1)) {
 		t.Errorf("min.UInt16(1) = uint16(1)")
 	}
 }
 
 func Test_min_UInt32(t *testing.T) {
-	if reflect.DeepEqual(Min.UInt32(1), uint32(1)) {
+	if !reflect.DeepEqual(Min.UInt32(1), uint32(1)) {
 		t.Errorf("min.UInt32(1) = uint32(1)")
 	}
 }
 
 func Test_min_UInt64(t *testing.T) {
-	if reflect.DeepEqual(Min.UInt64(1), uint64(1)) {
+	if !reflect.DeepEqual(Min.UInt64(1), uint64(1)) {
 		t.Errorf("min.UInt64(1) = uint64(1)")
 	}
 }
