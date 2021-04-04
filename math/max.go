014ef3ba5e31751a82e1de596116439ac42cf6c1
賀川　諒 <ryo.kagawa.217@gmail.com>
2021-04-04 23:07:07 +0900

    bugfix: math/maxでuint64で境界値を正常に判定できていなかった

--- math/max.go
+++ math/max.go
@@ -1,69 +1,125 @@
 package math
 
-import (
-	"math"
-	"reflect"
-)
-
 type max struct{}
 
 var Max = max{}
 
-func (m max) Interface(value interface{}, valueList interface{}) interface{} {
-	result := ToFloat64(value)
-	valueListReflectValue := reflect.ValueOf(valueList)
-	length := valueListReflectValue.Len()
-	for i := 0; i < length; i++ {
-		result = math.Max(result, ToFloat64(valueListReflectValue.Index(i).Interface()))
-	}
-
-	return reflect.ValueOf(result).Convert(reflect.ValueOf(value).Type()).Interface()
-}
-
 func (m max) Float32(value float32, valueList ...float32) float32 {
-	return m.Interface(value, valueList).(float32)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) Float64(value float64, valueList ...float64) float64 {
-	return m.Interface(value, valueList).(float64)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) Int(value int, valueList ...int) int {
-	return m.Interface(value, valueList).(int)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) Int8(value int8, valueList ...int8) int8 {
-	return m.Interface(value, valueList).(int8)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) Int16(value int16, valueList ...int16) int16 {
-	return m.Interface(value, valueList).(int16)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) Int32(value int32, valueList ...int32) int32 {
-	return m.Interface(value, valueList).(int32)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) Int64(value int64, valueList ...int64) int64 {
-	return m.Interface(value, valueList).(int64)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) UInt(value uint, valueList ...uint) uint {
-	return m.Interface(value, valueList).(uint)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) UInt8(value uint8, valueList ...uint8) uint8 {
-	return m.Interface(value, valueList).(uint8)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) UInt16(value uint16, valueList ...uint16) uint16 {
-	return m.Interface(value, valueList).(uint16)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) UInt32(value uint32, valueList ...uint32) uint32 {
-	return m.Interface(value, valueList).(uint32)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
 
 func (m max) UInt64(value uint64, valueList ...uint64) uint64 {
-	return m.Interface(value, valueList).(uint64)
+	result := value
+	for _, x := range valueList {
+		if result < x {
+			result = x
+		}
+	}
+	return result
 }
--- math/max_test.go
+++ math/max_test.go
@@ -1,131 +1,79 @@
 package math
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
 
-func Test_max_Interface(t *testing.T) {
-	type args struct {
-		value     interface{}
-		valueList interface{}
-	}
-	tests := []struct {
-		name string
-		m    max
-		args args
-		want interface{}
-	}{
-		{
-			name: "func(int(20), []int{}) == int(20)",
-			args: args{
-				value:     int(20),
-				valueList: []int{},
-			},
-			want: int(20),
-		},
-		{
-			name: "func(int(20), []int{10}) == int(20)",
-			args: args{
-				value:     int(20),
-				valueList: []int{10},
-			},
-			want: int(20),
-		},
-		{
-			name: "func(int(10), []int{20}) == int(20)",
-			args: args{
-				value:     int(10),
-				valueList: []int{20},
-			},
-			want: int(20),
-		},
-		{
-			name: "func(int(10), []int{20, 30, 25}) == int(30)",
-			args: args{
-				value:     int(10),
-				valueList: []float64{20, 30, 25},
-			},
-			want: int(30),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.m.Interface(tt.args.value, tt.args.valueList); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("max.Interface() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func Test_max_Float32(t *testing.T) {
-	if !reflect.DeepEqual(Max.Float32(1), float32(1)) {
-		t.Errorf("max.Float32(1) = float32(1)")
+	if !reflect.DeepEqual(Max.Float32(0, math.MaxFloat32), float32(math.MaxFloat32)) {
+		t.Errorf("max.Float32(0, math.MaxFloat32) = float32(math.MaxFloat32)")
 	}
 }
 
 func Test_max_Float64(t *testing.T) {
-	if !reflect.DeepEqual(Max.Float64(1), float64(1)) {
-		t.Errorf("max.Float64(1) = float64(1)")
+	if !reflect.DeepEqual(Max.Float64(0, math.MaxFloat64), float64(math.MaxFloat64)) {
+		t.Errorf("max.Float64(0, math.MaxFloat64) = float64(math.MaxFloat64)")
 	}
 }
 
 func Test_max_Int(t *testing.T) {
-	if !reflect.DeepEqual(Max.Int(1), int(1)) {
-		t.Errorf("max.Int(1) = int(1)")
+	if !reflect.DeepEqual(Max.Int(0, 1), int(1)) {
+		t.Errorf("max.Int(0, 1) = int(1)")
 	}
 }
 
 func Test_max_Int8(t *testing.T) {
-	if !reflect.DeepEqual(Max.Int8(1), int8(1)) {
-		t.Errorf("max.Int8(1) = int8(1)")
+	if !reflect.DeepEqual(Max.Int8(0, math.MaxInt8), int8(math.MaxInt8)) {
+		t.Errorf("max.Int8(0, math.MaxInt8) = int8(math.MaxInt8)")
 	}
 }
 
 func Test_max_Int16(t *testing.T) {
-	if !reflect.DeepEqual(Max.Int16(1), int16(1)) {
-		t.Errorf("max.Int16(1) = int16(1)")
+	if !reflect.DeepEqual(Max.Int16(0, math.MaxInt16), int16(math.MaxInt16)) {
+		t.Errorf("max.Int16(0, math.MaxInt16) = int16(math.MaxInt16)")
 	}
 }
 
 func Test_max_Int32(t *testing.T) {
-	if !reflect.DeepEqual(Max.Int32(1), int32(1)) {
-		t.Errorf("max.Int32(1) = int32(1)")
+	if !reflect.DeepEqual(Max.Int32(0, math.MaxInt32), int32(math.MaxInt32)) {
+		t.Errorf("max.Int32(0, math.MaxInt32) = int32(math.MaxInt32)")
 	}
 }
 
 func Test_max_Int64(t *testing.T) {
-	if !reflect.DeepEqual(Max.Int64(1), int64(1)) {
-		t.Errorf("max.Int64(1) = int64(1)")
+	if !reflect.DeepEqual(Max.Int64(0, math.MaxInt64), int64(math.MaxInt64)) {
+		t.Errorf("max.Int64(0, math.MaxInt64) = int64(math.MaxInt64)")
 	}
 }
 
 func Test_max_UInt(t *testing.T) {
-	if !reflect.DeepEqual(Max.UInt(1), uint(1)) {
-		t.Errorf("max.UInt(1) = uint(1)")
+	if !reflect.DeepEqual(Max.UInt(0, 1), uint(1)) {
+		t.Errorf("max.UInt(0, 1) = uint(1)")
 	}
 }
 
 func Test_max_UInt8(t *testing.T) {
-	if !reflect.DeepEqual(Max.UInt8(1), uint8(1)) {
-		t.Errorf("max.UInt8(1) = uint8(1)")
+	if !reflect.DeepEqual(Max.UInt8(0, math.MaxUint8), uint8(math.MaxUint8)) {
+		t.Errorf("max.UInt8(0, math.MaxUint8) = uint8(math.MaxUint8)")
 	}
 }
 
 func Test_max_UInt16(t *testing.T) {
-	if !reflect.DeepEqual(Max.UInt16(1), uint16(1)) {
-		t.Errorf("max.UInt16(1) = uint16(1)")
+	if !reflect.DeepEqual(Max.UInt16(0, math.MaxUint16), uint16(math.MaxUint16)) {
+		t.Errorf("max.UInt16(0, math.MaxUint16) = uint16(math.MaxUint16)")
 	}
 }
 
 func Test_max_UInt32(t *testing.T) {
-	if !reflect.DeepEqual(Max.UInt32(1), uint32(1)) {
-		t.Errorf("max.UInt32(1) = uint32(1)")
+	if !reflect.DeepEqual(Max.UInt32(0, math.MaxUint32), uint32(math.MaxUint32)) {
+		t.Errorf("max.UInt32(0, math.MaxUint32) = uint32(math.MaxUint32)")
 	}
 }
 
 func Test_max_UInt64(t *testing.T) {
-	if !reflect.DeepEqual(Max.UInt64(1), uint64(1)) {
-		t.Errorf("max.UInt64(1) = uint64(1)")
+	if !reflect.DeepEqual(Max.UInt64(0, math.MaxUint64), uint64(math.MaxUint64)) {
+		t.Errorf("max.UInt64(0, math.MaxUint64) = uint64(math.MaxUint64)")
 	}
 }
